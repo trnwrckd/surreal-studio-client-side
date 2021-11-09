9ee3a14947789b6134489ee4062cc5dd73b8b32a
Ishmamur Rahman <trnwrckd@gmail.com>
2021-11-10 00:31:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home/Home.js'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './Pages/Register/Register';
+import Login from './Pages/Login/Login';
+import Header from './Shared/Header/Header';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+            <Header></Header>
+            <Switch>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+            <Route path='/home'>
+              <Home></Home>
+            </Route>
+
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+
+            <Route path='/register'>
+              <Register></Register>
+            </Route>
+
+
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    );
+};
+
+export default Header;
